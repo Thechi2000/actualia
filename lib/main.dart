3c929cedc4dea2888fd99d7d651f625ef36b963a
Paul Madelénat <18498650+pmdlt@users.noreply.github.com>
2024-04-06 09:23:14 +0000

    chore: resolve conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:actualia/viewmodels/news_settings.dart';
-import 'package:actualia/views/news_settings.dart';
+import 'package:actualia/models/auth_model.dart';
+import 'package:actualia/views/home_view.dart';
+import 'package:actualia/views/login_view.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -11,20 +12,47 @@ Future<void> main() async {
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRweGRkYmp5amRzY3Z1aHd1dHd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA5NTQzNDcsImV4cCI6MjAyNjUzMDM0N30.0vB8huUmdJIYp3M1nMeoixQBSAX_w2keY0JsYj2Gt8c',
   );
   runApp(ChangeNotifierProvider(
-    create: (context) => NewsSettingsViewModel(),
-    child: App(),
+    create: (context) => AuthModel(),
+    child: const App(),
   ));
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    AuthModel authModel = Provider.of(context);
+
+    Widget body;
+    if (authModel.isSignedIn) {
+      body = const HomeView();
+    } else {
+      body = const LoginView();
+    }
+
     return MaterialApp(
-      title: 'My App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: OnboardingView(),
-    );
+        title: 'ActualIA',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: const Text('ActualIA'),
+          ),
+          body: body,
+        ));
   }
 }
