8e4fcc3fbd256c44f7202bb1c62ec71e58157488
Clément Josso <clement.josso@epfl.ch>
2024-04-10 13:31:42 +0200

    feat: add support for multiple news and improve general architecture

--- lib/widgets/news_text.dart
+++ lib/widgets/news_text.dart
@@ -13,56 +13,56 @@ class NewsText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              //Box containing the title and the date
-              padding: const EdgeInsets.symmetric(horizontal: 80.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    date,
-                    style: const TextStyle(
-                      color: Color(0xFFCDCDDC),
-                      fontSize: 8,
-                      fontFamily: 'Fira Code',
-                      fontWeight: FontWeight.w400,
-                    ),
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            //Box containing the title and the date
+            //NB : The alignment will change after we add the button to play the audio
+            padding: const EdgeInsets.symmetric(horizontal: 80.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  date,
+                  style: const TextStyle(
+                    color: Color(0xFFCDCDDC),
+                    fontSize: 8,
+                    fontFamily: 'Fira Code',
+                    fontWeight: FontWeight.w400,
                   ),
-                  Text(
-                    title,
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 22,
-                      fontFamily: 'EB Garamond',
-                      fontWeight: FontWeight.w400,
-                      height: 1.2,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 64.0, vertical: 20.0),
-              child: Text(
-                textBody,
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 12,
-                  fontFamily: 'Fira Code',
-                  fontWeight: FontWeight.w300,
                 ),
-                softWrap: true,
-              ),
+                Text(
+                  title,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 22,
+                    fontFamily: 'EB Garamond',
+                    fontWeight: FontWeight.w400,
+                    height: 1.2,
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 64.0, vertical: 20.0),
+            child: Text(
+              textBody,
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 12,
+                fontFamily: 'Fira Code',
+                fontWeight: FontWeight.w300,
+              ),
+              softWrap: true,
+              textAlign: TextAlign.justify,
+            ),
+          ),
+        ],
       ),
     );
   }
