9007202e1a50a49b597a156243be9447e10c69c9
Jacopo Moretti <quartztz@proton.me>
2024-04-11 18:36:44 +0200

    feat: implements behavior for login page, now gsignin and guestsignin work

--- lib/widgets/signin_controls.dart
+++ lib/widgets/signin_controls.dart
@@ -3,8 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 
-class SignInControls extends StatelessWidget {
-  const SignInControls({super.key});
+class SignInControls extends StatefulWidget {
+  const SignInControls({super.key}); 
+
+  @override
+  State<SignInControls> createState() => _SignInControls();
+}
+
+class _SignInControls extends State<SignInControls> {
+  String? _error; 
 
   @override
   Widget build(BuildContext context) {
@@ -18,25 +25,25 @@ class SignInControls extends StatelessWidget {
           child: Container(
             padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
             child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[ 
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(0, 0, 15, 0),
-                      child: googleLogo
-                    ),
-                    const Text(
-                      style: TextStyle(
-                        fontFamily: "EB Garamond", 
-                        fontWeight: FontWeight.w500, 
-                        fontSize: 18,
-                        color: Colors.black,
-                      ),
-                      "Sign in with Google"
-                    ),
-                  ]
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[ 
+                Container(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 15, 0),
+                  child: googleLogo
                 ),
+                const Text(
+                  style: TextStyle(
+                    fontFamily: "EB Garamond", 
+                    fontWeight: FontWeight.w500, 
+                    fontSize: 18,
+                    color: Colors.black,
+                  ),
+                  "Sign in with Google"
+                ),
+              ]
+             ),
           ),
           onPressed: () async { 
             await authModel.signInWithGoogle();
@@ -57,7 +64,13 @@ class SignInControls extends StatelessWidget {
               await authModel.signInWithFakeAccount();
             }
           )
-        )
+        ),
+        if (_error != null) ...<Widget>[
+          Text(
+            _error!,
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+        ],
       ]
     );
   }
