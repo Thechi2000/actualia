44e5a0f6b5b40462f35bda58608a0133249f27c5
Clément Josso <clement.josso@epfl.ch>
2024-04-11 17:35:33 +0200

    fix: update the news viewmodel to handle errors

--- lib/viewmodels/news.dart
+++ lib/viewmodels/news.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:actualia/models/news.dart';
 import 'package:flutter/foundation.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -8,39 +10,42 @@ class NewsViewModel extends ChangeNotifier {
   News? get news => _news;
 
   Future<void> fetchNews(DateTime date) async {
-    //final dateString = date.toIso8601String(); //TODO, find a way to format the date
-    final dateString = '2024-04-10';
-    print("Fetching news for $dateString");
-    print("User: ${supabase.auth.currentUser!.id}");
-    /*
-    final res = await supabase
-        .from('news')
-        .select()
-        .eq('user', supabase.auth.currentUser!.id)
-        .eq('date', dateString)
-        .single();
-        */
-    final res =
-        await supabase.from('news').select().single(); //FOR TESTING PURPOSES
-    print("Response: $res");
-    if (res['error'] != null) {
-      print("Error fetching news: ${res['error'].message}");
+    var dateString = date.toString().substring(0, 10);
+    try {
+      print(DateTime.parse('2024-04-14').weekday);
+      final res = await supabase
+          .from('news')
+          .select()
+          .eq('user', supabase.auth.currentUser!.id)
+          .eq('date', dateString)
+          .single();
+
+      final texts = res['texts'];
+      final sources = res['sources'];
+
+      _news = News(
+        title: res['title'],
+        date: res['date'],
+        paragraphs: List.generate(texts.length, (index) {
+          return Paragraph(
+            text: texts[index],
+            source: sources[index],
+          );
+        }),
+      );
+      notifyListeners();
+    } catch (e) {
+      log("Error fetching settings: $e");
+      _news = News(
+          date: date.toString().substring(0, 10),
+          title: 'No news found for this date',
+          paragraphs: [
+            Paragraph(
+                text: 'There has been an error fetching news from the database. (click me to print the error message)',
+                source: e.toString())
+          ]);
+      notifyListeners();
       return;
     }
-
-    final texts = res['texts'];
-    final sources = res['sources'];
-
-    _news = News(
-      title: res['title'],
-      date: res['date'],
-      paragraphs: List.generate(texts.length, (index) {
-        return Paragraph(
-          text: texts[index],
-          source: sources[index],
-        );
-      }),
-    );
-    notifyListeners();
   }
 }
