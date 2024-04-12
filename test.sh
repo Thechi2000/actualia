13d86048a97613f7392b3c96cc28f903ab814f4d
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-04-12 19:08:29 +0200

    ci: run tests in subfolders

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 sh scripts/import_files_coverage.sh actualia
 
 echo Running tests
-flutter test --coverage || exit 1
+flutter test --coverage test/widgets/*.dart test/unit/*.dart test/coverage_helper_test.dart || exit 1
 
 echo Generating coverage report
 genhtml coverage/lcov.info -o coverage/html
