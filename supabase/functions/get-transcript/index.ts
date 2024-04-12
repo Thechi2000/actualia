eb4679936e90a6ec6d86ff8eadc6daf90a3fd071
Paul Madelénat <18498650+pmdlt@users.noreply.github.com>
2024-04-12 17:33:19 +0000

    fix: typeError resolution

--- supabase/functions/get-transcript/index.ts
+++ supabase/functions/get-transcript/index.ts
@@ -29,7 +29,7 @@ interface ArticlesResponse {
   articles: Article[];
 }
 
-interface MergedData {
+interface MergedJSON {
   totalArticles: number;
   totalNewsByLLM: string;
   articles: (Article & News)[];
@@ -65,27 +65,22 @@ Deno.serve(async (request) => {
   )
     .filter("created_by", "eq", userId)
     .filter("wants_interests", "eq", true);
-  if (interests.data === null) {
+  if (interests.error) {
     console.error("We can't get the user's interests: " + interests.error);
     return new Response("Internal Server Error", { status: 500 });
   }
   console.log(interests);
 
-  const test = await supabaseClient.from("news_settings").select(
-    "*",
-  );
-  console.log(test);
-
   // Get the news from GNews.
-  //const gNews = await getGNews(interests);
-  const gNews = await getGNews("Computer Science");
+  const gNews = await getGNews(interests);
 
   // Generate a transcript from the news.
   const articlesPrompt = generateArticlesPrompt(gNews);
   const transcript = await generateTranscript(articlesPrompt);
 
   // Merge the GNews and the transcript.
-  const response = mergeJSON(gNews, transcript);
+  const response = mergeJSON(transcript, gNews);
+  console.log(response);
 
   // Insert the transcript into the database.
   const { data, error } = await supabaseClient.from("news").insert({
@@ -94,10 +89,6 @@ Deno.serve(async (request) => {
     transcript: response,
   });
 
-  if (error !== null) {
-    console.error("Failed to insert the transcript: " + error);
-    return new Response("Internal Server Error", { status: 500 });
-  }
   console.log(data);
 
   // return transcript
@@ -118,13 +109,11 @@ async function generateTranscript(articlesPrompt: any) {
       {
         "role": "system",
         "content":
-          'You are a journalist who provide a transcript from a selection of news that we give you right after. Create a valid JSON array from this news-cut transcript, as in the example here {"nb_news":"The number of news you proceed (ex. 4)","full_transcript":"This is the global summary. For exemple you can say hello, and continue with the summary of the news 1, which makes a valid transition to the news that follows, as the text will be displayed in full without interruption. And when it\'s finished, who can say for exemple see you tomorrow!","news":[{"transcript":"the summary of the news 1, which makes a valid transition to","titre":"The title of the news 1"},{"transcript":"the news that follows, as the text will be displayed in full without interruption.","titre":"The title of the news 2"}]} The full_transcript need to correspond EXACTLY to the sum of the strings transcript',
+          'You are a journalist who provide a transcript from a selection of article headlines that we give you. It\'s up to you to compose your own text according to these, and to make interesting transitions between transcript items (IMPORTANT). For exemple, you can use Also, On the other hand, etc. to do the transition between news. Create a valid JSON array from this news-cut transcript, as in the example here {"totalNewsByLLM":"The number of news you proceed (ex. 3 here)","news":[{"transcript":"The summary of the news 1"},{"transcript":"The summary of the news 2"},{"transcript":"etc."}]}',
       },
       {
         "role": "user",
-        "content":
-          "In this list, we give you the article headlines. It's up to you to compose your own text according to these, and to make interesting transitions between news items (IMPORTANT). For exemple, you can use Also, On the other hand, etc. to do the transition between news. Please do so in the order of the following news items:\n\n" +
-          articlesPrompt,
+        "content": articlesPrompt,
       },
     ],
   });
@@ -142,12 +131,17 @@ async function getGNews(interests: any) {
   // Computes the GNews query by ORing all the categories.
   // The categories must be "escaped" by putting them in quotes.
   // See https://gnews.io/docs/v4#search-endpoint-query-parameters for more details.
-  const gNewsQuery = interests.data.length > 0
+  let gNewsQuery = interests && interests.data && interests.data.length > 0 &&
+      interests.data[0].interests
     ? (interests.data[0].interests as string[]).map((s) => `"${s}"`).join(
       " OR ",
     )
     : "";
 
+  if (!gNewsQuery) {
+    console.error("No user's interests, we will use default ones.");
+    gNewsQuery = "Microsoft OR OpenAI";
+  }
   // Constructs the query URL.
   const url = `https://gnews.io/api/v4/search?q=${
     encodeURIComponent(gNewsQuery)
@@ -170,16 +164,16 @@ function generateArticlesPrompt(json: ArticlesResponse): string {
   return result;
 }
 
-function mergeJSON(json1: MergedData, json2: MergedData): MergedData {
-  const mergedData: MergedData = {
+function mergeJSON(
+  json1: MergedJSON,
+  json2: { totalArticles: number; articles: Article[] },
+): MergedJSON {
+  console.log(json2);
+  return {
     totalArticles: json2.totalArticles,
     totalNewsByLLM: json1.totalNewsByLLM,
-    articles: json2.articles.map((article, index) => {
-      return { ...article, ...json1.articles[index] };
-    }),
+    articles: json2.articles.map((article) => ({ ...article, transcript: "" })),
   };
-
-  return mergedData;
 }
 
 // Verify the JSON structure of the OpenAI response
