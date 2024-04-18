47ce114e0a8a5e4f0ea155982a5d2633f29547d2
Leander <leander.cretegny@epfl.ch>
2024-04-18 23:33:07 +0200

    fix: fix the duplicate button in appbar

--- lib/widgets/top_app_bar.dart
+++ lib/widgets/top_app_bar.dart
@@ -13,19 +13,12 @@ class TopAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    Widget leftCorner;
-    if (enableReturnButton) {
-      leftCorner = ReturnButton(onPressed!);
-    } else {
-      leftCorner = const SizedBox(width: 10);
-    }
-
     return AppBar(
       title: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           //Invisible box to center the title
-          leftCorner,
+          const SizedBox(width: 10),
           // Central button acting as the application title
           Expanded(
             child: TextButton(
@@ -65,22 +58,4 @@ class TopAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Size get preferredSize => const Size.fromHeight(48);
-}
-
-class ReturnButton extends StatelessWidget {
-  final void Function() onPressed;
-
-  const ReturnButton(this.onPressed, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onPressed,
-      borderRadius: BorderRadius.circular(16.0),
-      child: const Padding(
-        padding: EdgeInsets.fromLTRB(8, 8, 8, 8),
-        child: Icon(Icons.arrow_back_ios_new),
-      ),
-    );
-  }
 }
