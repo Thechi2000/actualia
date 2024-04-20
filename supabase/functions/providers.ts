669d11ee763fa21c5dfe1635119aef398256a536
Ludovic Mermod <ludovic.mermod@gmail.com>
2024-04-21 01:42:12 +0200

    feat: implement gnews in fetchNews

--- supabase/functions/providers.ts
+++ supabase/functions/providers.ts
@@ -1,6 +1,19 @@
 import { News, NewsSettings, Provider } from "../functions/model.ts";
 import { parseFeed } from "https://deno.land/x/rss@1.0.2/mod.ts";
 
+interface GNewsOutput {
+  totalArticles: number;
+  articles: GNewsItem[];
+}
+interface GNewsItem {
+  title: string;
+  description: string;
+  content: string;
+  url: string;
+  image: string;
+  publishedAt: string;
+}
+
 /**
  * Fetch and normalize news from a given provider.
  * @param provider The provider where to fetch the news.
@@ -13,10 +26,40 @@ export async function fetchNews(
 ): Promise<News[]> {
   switch (provider.type) {
     case "gnews": {
-      return [];
+      console.info("Fetching from GNews");
+
+      const topics: string[] = [];
+      if (newsSettings.wants_interests) {
+        topics.push(newsSettings.interests);
+      }
+      if (newsSettings.wants_countries) {
+        topics.push(newsSettings.countries);
+      }
+      if (newsSettings.wants_cities) {
+        topics.push(newsSettings.cities);
+      }
+
+      const query = topics.map((s) => `"${s}"`).join(" OR ");
+      const url = `https://gnews.io/api/v4/search?q=${
+        encodeURIComponent(query)
+      }&apikey=${
+        encodeURIComponent(Deno.env.get("GNEWS_API_KEY") || "")
+      }&from=${
+        encodeURIComponent(new Date(Date.now() - 86400000).toISOString())
+      }&lang=en`;
+
+      const news: GNewsOutput = await (await fetch(url)).json();
+
+      return news.articles.map((a) => ({
+        title: a.title,
+        description: a.description,
+        link: a.url,
+        date: new Date(a.publishedAt),
+      }));
     }
     case "rss": {
       console.info("Fetching RSS from ", provider.url);
+
       const response = await fetch(
         provider.url,
       );
