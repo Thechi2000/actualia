d2bc1e76c35fe79f6c561bf851adae69300491a0
Ludovic Mermod <ludovic.mermod@gmail.com>
2024-04-21 10:09:08 +0200

    feat: allow to fetch multiple providers

--- supabase/functions/providers.ts
+++ supabase/functions/providers.ts
@@ -16,68 +16,81 @@ interface GNewsItem {
 
 /**
  * Fetch and normalize news from a given provider.
- * @param provider The provider where to fetch the news.
+ * @param provider One or several providers where to fetch the news.
  * @param newsSettings The settings to filter the fetched news.
  * @returns The news of today from that provider.
  */
 export async function fetchNews(
-  provider: Provider,
+  provider: Provider | Provider[],
   newsSettings: NewsSettings,
 ): Promise<News[]> {
-  switch (provider.type) {
-    case "gnews": {
-      console.info("Fetching from GNews");
+  async function singleFetch(
+    provider: Provider,
+    newsSettings: NewsSettings,
+  ): Promise<News[]> {
+    switch (provider.type) {
+      case "gnews": {
+        console.info("Fetching from GNews");
 
-      const topics: string[] = [];
-      if (newsSettings.wants_interests) {
-        topics.push(newsSettings.interests);
+        const topics: string[] = [];
+        if (newsSettings.wants_interests) {
+          topics.push(newsSettings.interests);
+        }
+        if (newsSettings.wants_countries) {
+          topics.push(newsSettings.countries);
+        }
+        if (newsSettings.wants_cities) {
+          topics.push(newsSettings.cities);
+        }
+
+        const query = topics.map((s) => `"${s}"`).join(" OR ");
+        const url = `https://gnews.io/api/v4/search?q=${
+          encodeURIComponent(query)
+        }&apikey=${
+          encodeURIComponent(Deno.env.get("GNEWS_API_KEY") || "")
+        }&from=${
+          encodeURIComponent(new Date(Date.now() - 86400000).toISOString())
+        }&lang=en`;
+
+        const news: GNewsOutput = await (await fetch(url)).json();
+
+        return news.articles.map((a) => ({
+          title: a.title,
+          description: a.description,
+          link: a.url,
+          date: new Date(a.publishedAt),
+        }));
       }
-      if (newsSettings.wants_countries) {
-        topics.push(newsSettings.countries);
+      case "rss": {
+        console.info("Fetching RSS from ", provider.url);
+
+        const response = await fetch(
+          provider.url,
+        );
+        const xml = await response.text();
+        const feed = await parseFeed(xml);
+
+        feed.entries.forEach((e) => console.log(e));
+        return feed.entries.map((i) => ({
+          title: i.title?.value || "",
+          description: i.description?.value || "",
+          date: i.published || new Date(Date.now()),
+          link: i.links[0].href || "",
+        }))
+          .filter((i: News) => (Date.now() - i.date.getTime()) < 86400000);
       }
-      if (newsSettings.wants_cities) {
-        topics.push(newsSettings.cities);
-      }
-
-      const query = topics.map((s) => `"${s}"`).join(" OR ");
-      const url = `https://gnews.io/api/v4/search?q=${
-        encodeURIComponent(query)
-      }&apikey=${
-        encodeURIComponent(Deno.env.get("GNEWS_API_KEY") || "")
-      }&from=${
-        encodeURIComponent(new Date(Date.now() - 86400000).toISOString())
-      }&lang=en`;
-
-      const news: GNewsOutput = await (await fetch(url)).json();
-
-      return news.articles.map((a) => ({
-        title: a.title,
-        description: a.description,
-        link: a.url,
-        date: new Date(a.publishedAt),
-      }));
+      default:
+        throw new Error(
+          `Unknown provider: ${(provider as { type: string }).type}`,
+        );
     }
-    case "rss": {
-      console.info("Fetching RSS from ", provider.url);
+  }
 
-      const response = await fetch(
-        provider.url,
-      );
-      const xml = await response.text();
-      const feed = await parseFeed(xml);
-
-      feed.entries.forEach((e) => console.log(e));
-      return feed.entries.map((i) => ({
-        title: i.title?.value || "",
-        description: i.description?.value || "",
-        date: i.published || new Date(Date.now()),
-        link: i.links[0].href || "",
-      }))
-        .filter((i: News) => (Date.now() - i.date.getTime()) < 86400000);
-    }
-    default:
-      throw new Error(
-        `Unknown provider: ${(provider as { type: string }).type}`,
-      );
+  if (Array.isArray(provider)) {
+    return (await Promise.all(
+      provider.map((p) => singleFetch(p, newsSettings)),
+    )).flat();
+  } else {
+    return await singleFetch(provider, newsSettings);
   }
 }
