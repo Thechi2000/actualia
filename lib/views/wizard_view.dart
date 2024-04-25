aaa6789c5e62d1b4f395569c3d3f8740eb1bbf98
Leander <leander.cretegny@epfl.ch>
2024-04-25 14:33:09 +0200

    refactor: make wizard_view code more coherent and remove unused import

--- lib/views/wizard_view.dart
+++ lib/views/wizard_view.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:actualia/models/news_settings.dart';
 import 'package:actualia/viewmodels/news_settings.dart';
 import 'package:flutter/material.dart';
@@ -39,6 +37,67 @@ class _WizardViewState extends State<WizardView> {
       _interests = fromDB.interests;
     }
 
+    PreferredSizeWidget appBar = PreferredSize(
+        preferredSize: const Size.fromHeight(120.0),
+        child: Container(
+          margin: const EdgeInsets.fromLTRB(16, 50, 16, 8.0),
+          child: const Text(
+            "Tell us more about your interests",
+            textScaler: TextScaler.linear(2.0),
+            style: TextStyle(
+                fontFamily: 'EB Garamond',
+                fontWeight: FontWeight.w700,
+                color: Colors.black),
+            maxLines: 2,
+          ),
+        ));
+
+    Widget body = Container(
+      margin: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+      child: ListView(
+        /// temp mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          //each formField is an entry in the wizard
+          SelectorWithInstruction(
+                (value) {
+              setState(() {
+                _countries = value;
+              });
+            },
+            "Select some countries",
+            newsSettingsDefault.predefinedCountries,
+            "Country",
+            selectedItems: _countries,
+            key: const Key("country-selector"),
+          ),
+          SelectorWithInstruction(
+                (value) {
+              setState(() {
+                _cities = value;
+              });
+            },
+            "Select some cities",
+            newsSettingsDefault.predefinedCities,
+            "City",
+            selectedItems: _cities,
+            key: const Key("city-selector"),
+          ),
+          SelectorWithInstruction(
+                (value) {
+              setState(() {
+                _interests = value;
+              });
+            },
+            "Select some interests",
+            newsSettingsDefault.predefinedInterests,
+            "Interests",
+            selectedItems: _interests,
+            key: const Key("interest-selector"),
+          )
+        ],
+      ),
+    );
+
     Widget bottomBar = WizardNavigationBottomBar(
         showLeft: widget.fromSetting,
         lText: 'Cancel',
@@ -65,65 +124,9 @@ class _WizardViewState extends State<WizardView> {
         });
 
     return Scaffold(
-        appBar: PreferredSize(
-            preferredSize: const Size.fromHeight(120.0),
-            child: Container(
-              margin: const EdgeInsets.fromLTRB(16, 50, 16, 8.0),
-              child: const Text(
-                "Tell us more about your interests",
-                textScaler: TextScaler.linear(2.0),
-                style: TextStyle(
-                    fontFamily: 'EB Garamond',
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black),
-                maxLines: 2,
-              ),
-            )),
-        body: Container(
-          margin: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
-          child: ListView(
-            /// temp mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              //each formField is an entry in the wizard
-              SelectorWithInstruction(
-                (value) {
-                  setState(() {
-                    _countries = value;
-                  });
-                },
-                "Select some countries",
-                newsSettingsDefault.predefinedCountries,
-                "Country",
-                selectedItems: _countries,
-                key: const Key("country-selector"),
-              ),
-              SelectorWithInstruction(
-                (value) {
-                  setState(() {
-                    _cities = value;
-                  });
-                },
-                "Select some cities",
-                newsSettingsDefault.predefinedCities,
-                "City",
-                selectedItems: _cities,
-                key: const Key("city-selector"),
-              ),
-              SelectorWithInstruction(
-                (value) {
-                  setState(() {
-                    _interests = value;
-                  });
-                },
-                "Select some interests",
-                newsSettingsDefault.predefinedInterests,
-                "Interests",
-                selectedItems: _interests,
-                key: const Key("interest-selector"),
-              )
-            ],
-          ),
-        ),
-        bottomNavigationBar: bottomBar);
+        appBar: appBar,
+        body: body,
+        bottomNavigationBar: bottomBar
+    );
   }
 }
