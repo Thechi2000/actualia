e8866582d31bfec7b2892b586f6d88c0aee6720a
Paul Madelénat <18498650+pmdlt@users.noreply.github.com>
2024-04-28 20:03:42 +0000

    feat: complete alarm model

--- lib/models/alarms.dart
+++ lib/models/alarms.dart
@@ -1,3 +1,6 @@
+import 'package:alarm/alarm.dart';
+import 'package:alarm/model/alarm_settings.dart';
+
 class Alarms {
   bool enabled;
   int id;
@@ -40,17 +43,33 @@ class Alarms {
       enableNotificationOnKill: true,
     );
   }
-}
 
-/* final alarmSettings = AlarmSettings(
-  id: 42,
-  dateTime: dateTime,
-  assetAudioPath: 'assets/alarm.mp3',
-  loopAudio: true,
-  vibrate: true,
-  volume: 0.8,
-  fadeDuration: 3.0,
-  notificationTitle: 'This is the title',
-  notificationBody: 'This is the body',
-  enableNotificationOnKill: true,
-); */
+  Future<void> setAlarm() async {
+    await Alarm.init();
+    final alarmSettings = AlarmSettings(
+      id: id,
+      dateTime: dateTime,
+      assetAudioPath: assetAudioPath,
+      loopAudio: loopAudio,
+      vibrate: vibrate,
+      volume: volume,
+      fadeDuration: fadeDuration,
+      notificationTitle: notificationTitle,
+      notificationBody: notificationBody,
+      enableNotificationOnKill: enableNotificationOnKill,
+    );
+    await Alarm.set(alarmSettings: alarmSettings);
+  }
+
+  Future<void> stopAlarm() async {
+    await Alarm.stop(id);
+  }
+
+  void onRingCallback() {
+    void yourOnRingCallback() {
+      // Do something when the alarm rings
+    }
+
+    Alarm.ringStream.stream.listen((_) => yourOnRingCallback());
+  }
+}
