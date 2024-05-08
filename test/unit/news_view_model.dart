37589046611ef9cba626cb4d40f96f273be09456
Paul Madelénat <18498650+pmdlt@users.noreply.github.com>
2024-05-08 17:39:59 +0000

    test: fix tests for fulltranscript impl

--- test/unit/news_view_model.dart
+++ test/unit/news_view_model.dart
@@ -95,7 +95,8 @@ class AlreadyExistingNewsVM extends NewsViewModel {
               date: "12-04-2024",
               content: "content",
               url: "url")
-        ]));
+        ],
+        fullTranscript: "fullTranscript"));
     return Future.value();
   }
 
@@ -113,20 +114,23 @@ class NonExistingNewsVM extends NewsViewModel {
   @override
   Future<void> fetchNews(DateTime date) {
     if (invokedTranscriptFunction) {
-      setNews(News(
-          date: DateTime.now().toIso8601String(),
-          title: "News",
-          transcriptId: -1,
-          audio: null,
-          paragraphs: [
-            Paragraph(
-                transcript: "text",
-                source: "source",
-                title: "title",
-                date: "12-04-2024",
-                content: "content",
-                url: "url")
-          ]));
+      setNews(
+        News(
+            date: DateTime.now().toIso8601String(),
+            title: "News",
+            transcriptId: -1,
+            audio: null,
+            paragraphs: [
+              Paragraph(
+                  transcript: "text",
+                  source: "source",
+                  title: "title",
+                  date: "12-04-2024",
+                  content: "content",
+                  url: "url")
+            ],
+            fullTranscript: "fullTranscript"),
+      );
     } else {
       setNews(null);
     }
@@ -170,7 +174,8 @@ class NewsListVM extends NewsViewModel {
               date: "12-04-2024",
               content: "content",
               url: "url")
-        ]));
+        ],
+        fullTranscript: "fullTranscript"));
     return Future.value();
   }
 
@@ -405,7 +410,8 @@ void main() {
               date: "12-04-2024",
               content: "content",
               url: "url")
-        ]));
+        ],
+        fullTranscript: "fullTranscript"));
     expect(vm.generateAudioCalled, isTrue);
   });
 }
