a6497a8210e20427100c0c8276f3e1aa61c9fc27
leander <leander.cretegny@epfl.ch>
2024-05-10 01:32:18 +0200

    chore: save progress

---
+++ lib/widgets/navigation_menu.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ActualiaBottomNavigationBar extends StatefulWidget {
+  const ActualiaBottomNavigationBar({super.key});
+
+  @override
+  State<ActualiaBottomNavigationBar> createState() =>
+      _ActualiaBottomNavigationBar();
+}
+
+class _ActualiaBottomNavigationBar extends State<ActualiaBottomNavigationBar> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final dest = NavigationDestination(icon: icon, label: label);
+
+    return;
+  }
+}
