a8cc710d2bd59a3ea9d29740c5f8c7b9aed02d68
Paul Madelénat <18498650+pmdlt@users.noreply.github.com>
2024-05-09 21:24:02 +0000

    feat: add text processing capabilities for images

--- supabase/functions/process-image/index.ts
+++ supabase/functions/process-image/index.ts
@@ -1,29 +1,63 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
+import {
+  isString,
+  validate,
+} from "https://deno.land/x/validasaur@v0.15.0/mod.ts";
+import { assertHasEnv } from "../util.ts";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2.42.4";
+import OpenAI from "https://deno.land/x/openai@v4.33.0/mod.ts";
 
-console.log("Hello from Functions!")
+const bodySchema = {
+  textFromImage: isString,
+};
 
-Deno.serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
+Deno.serve(async (request) => {
+  // Check that the required environment variables are available.
+  assertHasEnv("OPENAI_API_KEY");
+
+  let textFromImage: string = "";
+  try {
+    const body = await request.json();
+    const [passes, errors] = await validate(body, bodySchema);
+    if (!passes) {
+      console.log(`Invalid body: ${JSON.stringify(errors)}`);
+      return new Response(JSON.stringify({ errors }), { status: 400 });
+    }
+    textFromImage = body.textFromImage;
+  } catch (_) {}
+
+  // Create a Supabase client with the user's token.
+  const authHeader = request.headers.get("Authorization")!;
+  const supabaseClient = createClient(
+    Deno.env.get("SUPABASE_URL") ?? "",
+    Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+    { global: { headers: { Authorization: authHeader } } },
+  );
+
+  // Get the user from the token.
+  const user = await supabaseClient.auth.getUser();
+  if (user.error !== null) {
+    console.error(user.error);
+    return new Response("Authentication error", { status: 401 });
   }
 
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
-})
+  const userId = user.data.user.id;
 
-/* To invoke locally:
+  const openai = new OpenAI();
+  const completion = await openai.chat.completions.create({
+    "model": "gpt-3.5-turbo",
+    "messages": [
+      {
+        "role": "system",
+        "content":
+          "The user provides you an OCR text (not necessarily perfectly formatted) of a photo from a newspaper, or some other news item. The news is not recent, so you should be able to find it. If this doesn't mean anything to you, simply return a message saying you don't know the facts. If you do know the facts, then you give as much context as you can on them, relate the historical facts, the people involved, and so on.",
+      },
+      {
+        "role": "user",
+        "content": textFromImage,
+      },
+    ],
+  });
+  const fullTranscript = completion.choices[0].message.content;
 
-  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
-  2. Make an HTTP request:
-
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/process-image' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-    --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
-
-*/
+  return new Response(fullTranscript, { status: 200 });
+});
