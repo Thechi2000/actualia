61939846d3c96db374a2ae3528d354a88e54aa4a
leander <leander.cretegny@epfl.ch>
2024-05-11 17:30:05 +0200

    fix: fix tests after merge

--- test/widgets/profile_view.dart
+++ test/widgets/profile_view.dart
@@ -211,46 +211,6 @@ void main() {
     expect(find.text("Interests"), findsOne);
   });
 
-  testWidgets(
-      "Sources button work as intended and provider wizard is correctly displayed",
-      (tester) async {
-    NewsProvider google = GNewsProvider();
-    String url = "https://dummy.com";
-    NewsProvider rss = RSSFeedProvider(url: url);
-    ProvidersViewModel pvm = MockProvidersViewModel(
-        init: [(google, google.displayName()), (rss, rss.displayName())]);
-    await tester.pumpWidget(ProfilePageWrapper(
-        const ProfilePageView(),
-        MockNewsSettingsViewModel(),
-        pvm,
-        MockAuthModel(FakeSupabaseClient(), FakeGoogleSignin()),
-        MockAlarmsViewModel(FakeSupabaseClient())));
-
-    await tester.tap(find.text("Sources"));
-    await tester.pumpAndSettle();
-
-    //check provider wizard on screen
-    expect(find.byType(ProvidersWizardView), findsOneWidget);
-    await tester.tap(find.text("Next"));
-    await tester.pump();
-
-    expect(find.byType(RSSSelector), findsOneWidget);
-    String url2 = "https://test.com";
-    await tester.enterText(find.byType(TextField), url2);
-    await tester.testTextInput.receiveAction(TextInputAction.done);
-    await tester.pump();
-    expect(find.text(rss.displayName()), findsOne);
-    await tester.tap(find.text("Finish"));
-    await tester.pump();
-
-    expect(find.byType(ProfilePageView), findsOne);
-    List<(NewsProvider, String)> providers = pvm.newsProviders!;
-    expect(providers.contains((google, google.displayName())), isTrue);
-    expect(providers.contains((rss, rss.displayName())), isTrue);
-    NewsProvider rss2 = RSSFeedProvider(url: url2);
-    expect(providers.contains((rss2, rss2.displayName())), isTrue);
-  });
-
   testWidgets("Alarm button work as intended", (tester) async {
     await tester.pumpWidget(ProfilePageWrapper(
         const ProfilePageView(),
