b516a1a4a8eb4928a456fe7b20d7dfc54e33c5fd
Clément Josso <clement.josso@epfl.ch>
2024-05-16 03:15:42 +0200

    feat: add new view for context from OCR

--- lib/views/context_view.dart
+++ lib/views/context_view.dart
@@ -1,41 +1,31 @@
-import 'package:actualia/views/loading_view.dart';
+import 'package:actualia/utils/themes.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-//TODO: Everything, this is merely a placeholder, this PR needs 3 other PRs to be merged first in order to be implemented
-
-class ContextView extends StatefulWidget {
-  final XFile? image;
-  const ContextView({super.key, this.image});
-
-  @override
-  State<ContextView> createState() => _ContextViewState();
-}
-
-class _ContextViewState extends State<ContextView> {
-  @override
-  void initState() {
-    super.initState();
-    Future.microtask(() =>
-        Provider.of<NewsRecognitionViewModel>(context, listen: false)
-            .Ocr(widget.image));
-  }
+class ContextView extends StatelessWidget {
+  final String text;
+  const ContextView({super.key, required this.text});
 
   @override
   Widget build(BuildContext context) {
-    Widget loading =
-        const LoadingView(text: "The image is being processed. Please wait.");
-
-    final newsRecognitionViewModel =
-        Provider.of<NewsRecognitionViewModel>(context);
-    final context = newsRecognitionViewModel.context;
-    Widget body;
-    if (contex == null) {
-      body = loading;
-    } else {
-      body = const Text("TO BE IMPLEMENTED");
-    }
-
-    return body;
+    return ListView(
+      shrinkWrap: true,
+      children: <Widget>[
+        Container(
+            padding: const EdgeInsets.fromLTRB(
+                UNIT_PADDING * 2, UNIT_PADDING * 2, UNIT_PADDING * 2, 0),
+            child: Text(
+              "Here is some context regarding the text you just photographed.",
+              style: Theme.of(context)
+                  .textTheme
+                  .titleMedium!
+                  .copyWith(height: 1.2),
+            )),
+        Container(
+            padding: const EdgeInsets.all(UNIT_PADDING * 2),
+            child: Text(text,
+                style: Theme.of(context).textTheme.displaySmall,
+                textAlign: TextAlign.center)),
+      ],
+    );
   }
 }
