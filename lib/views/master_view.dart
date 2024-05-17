3f031508c04ae4c2b4179900a3f87b8c21493561
Paul Madelénat <18498650+pmdlt@users.noreply.github.com>
2024-05-17 18:24:28 +0000

    feat: implement sharring button in the new master_view concept

--- lib/views/master_view.dart
+++ lib/views/master_view.dart
@@ -1,13 +1,17 @@
 import 'dart:developer';
 
+import 'package:actualia/viewmodels/news.dart';
 import 'package:actualia/viewmodels/news_recognition.dart';
 import 'package:actualia/views/context_view.dart';
 import 'package:actualia/views/news_view.dart';
+import 'package:actualia/widgets/share_button.dart';
 import 'package:actualia/widgets/top_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:logging/logging.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:share_plus/share_plus.dart';
 import '../models/navigation_menu.dart';
 import '../widgets/navigation_menu.dart';
 
@@ -64,10 +68,35 @@ class _MasterView extends State<MasterView> {
 
   @override
   Widget build(BuildContext context) {
+    var firstTranscript = Provider.of<NewsViewModel>(context).newsList.first;
+
     Widget body;
+    Widget button = const SizedBox();
     switch (_currentViews) {
       case Views.NEWS:
         body = const NewsView();
+        button = ExpandableFab(
+          distance: 112,
+          children: [
+            ActionButton(
+              onPressed: () => Share.share(firstTranscript.fullTranscript),
+              icon: const Icon(Icons.text_fields),
+            ),
+            ActionButton(
+              onPressed: () async => await Share.shareXFiles([
+                XFile(
+                    // ignore: use_build_context_synchronously
+                    '${(await getApplicationDocumentsDirectory()).path}/audios/${firstTranscript.transcriptId}.mp3')
+              ], text: 'Check my personalized news audio!'),
+              icon: const Icon(Icons.audiotrack),
+            ),
+            ActionButton(
+              onPressed: () => Share.share(
+                  'https://actualia.app/shared/${firstTranscript.transcriptId}'),
+              icon: const Icon(Icons.link),
+            ),
+          ],
+        );
         break;
       case Views.CAMERA:
         body = const Center(child: Text("To be implemented"));
@@ -83,12 +112,14 @@ class _MasterView extends State<MasterView> {
         break;
     }
 
+    var newsViewModel;
     Widget screen = Scaffold(
       appBar: const TopAppBar(),
       bottomNavigationBar: ActualiaBottomNavigationBar(
         destinations: _destinations,
       ),
       body: body,
+      floatingActionButton: button,
     );
 
     return screen;
