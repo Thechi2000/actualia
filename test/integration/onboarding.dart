8543849ee71dd0e8bab1cc6f39ffa2ab23b7f045
Paul Madelénat <18498650+pmdlt@users.noreply.github.com>
2024-05-20 10:41:28 +0000

    test: update pre-onboarding skip

--- test/integration/onboarding.dart
+++ test/integration/onboarding.dart
@@ -94,13 +94,7 @@ void main() async {
     await tester.pumpAndSettle();
 
     // Pass the pre-onboarding view
-    await tester.tap(find.text("Next"));
-    await tester.pumpAndSettle();
-    await tester.tap(find.text("Next"));
-    await tester.pumpAndSettle();
-    await tester.tap(find.text("Next"));
-    await tester.pumpAndSettle();
-    await tester.tap(find.text("Done"));
+    await tester.tap(find.text("Skip"));
     await tester.pumpAndSettle();
 
     // Util function to select an interest entry from a given list.
